b835971a99a1b9986e40bbaf95eaf0654a5b3ec4
tetsu-upstr <bodomu@gmail.com>
2020-10-18 06:44:29 +0900

    mod controller

--- Controllers/CharacterContoroller.cs
+++ Controllers/CharacterContoroller.cs
@@ -1,23 +1,30 @@
-using System.Collections.Generic;
-using dotnet_rpg.Models;
+﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using dotnet_rpg.Models;
+using System.Linq;
 
 namespace dotnet_rpg.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class CharacterContoroller : ControllerBase
+    public class CharacterController : ControllerBase
     {
-        // private static Character knight = new Character();
         private static List<Character> characters = new List<Character> {
             new Character(),
-            new Character { Name = "Sam" }
+            new Character { Id = 1, Name = "Sam" }
         };
 
+        [HttpGet("GetAll")] // localhost5000:/character/getall がURLになる
         public IActionResult Get()
         {
             // return Ok(knight);
             return Ok(characters);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetSingle(int id)
+        {
+            return Ok(characters.FirstOrDefault(c => c.Id == id));
         }
     }
 }
